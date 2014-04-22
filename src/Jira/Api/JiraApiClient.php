d34974d6f366d31f0fa9968b60c4cd17d98b7711
Jonathan Webb <jonathan.webb@acquia.com>
2014-04-22 08:59:50 -0400

    update with latest revisions to code

--- src/Jira/Api/JiraApiClient.php
+++ src/Jira/Api/JiraApiClient.php
@@ -2,7 +2,95 @@
 
 namespace Jira\Api;
 
+use Guzzle\Common\Collection;
+use Jira\Common\JiraClientAuthPlugin;
+
 class JiraApiClient extends JiraApiClientV2
 {
+    const BASE_URL = 'http://localhost:2990/jira';
+    const BASE_PATH = '/rest/api/2';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Jira\Common\JiraClient
+     */
+    public static function factory($config = array())
+    {
+        $required = array(
+            'authentication_method',
+            'authentication_key',
+            'authentication_value',
+            'base_url',
+            'base_path',
+        );
+
+        $defaults = array(
+            'base_url' => self::BASE_URL,
+            'base_path' => self::BASE_PATH,
+        );
+
+        if (isset($config['authentication_method']) && $config['authentication_method'] == 'token') {
+            $defaults['authentication_value'] = 'api_token';
+        }
+
+        $config = Collection::fromConfig($config, $defaults, $required);
+        $client = new static($config->get('base_url'), $config);
+        $client->setDefaultHeaders(array(
+                'Content-Type' => 'application/json; charset=utf-8',
+            ));
+
+        $plugin = new JiraClientAuthPlugin($config->get('authentication_key'), $config->get('authentication_value'));
+        $client->addSubscriber($plugin);
+
+        return $client;
+    }
+
+    public function issue($issue)
+    {
+        $data = $this->sendGet('{+base_path}/issue/{+issue}?fields=id,key', array('issue' => $issue));
+        return $data;
+    }
+
+    public function search($jql, $options = array())
+    {
+        $data = $this->sendGet('{+base_path}/search?jql={+jql}&fields=id,key,summary', array('jql' => ($jql)));
+        return $data;
+    }
+
+    public function getIssueCreateMeta($projectKeys = null, $issuetypeIds = null)
+    {
+        $path = '{+base_path}/issue/createmeta?';
+        $variables = array();
+        if ($projectKeys) {
+            $path .= 'projectKeys={+projectKeys}&';
+            $variables['projectKeys'] = $projectKeys;
+        }
+        if ($issuetypeIds) {
+            $path .= 'issuetypeIds={+issuetypeIds}&';
+            $variables['issuetypeIds'] = $issuetypeIds;
+        }
+        $path .= 'expand=projects.issuetypes.fields';
+        $data = $this->sendGet($path, $variables);
+        return $data;
+    }
+
+    public function createIssue($options = array())
+    {
+        $path = '{+base_path}/issue';
+        $variables = array();
+        $body = json_encode(array('fields' => $options));
+        return $this->sendPost($path, $variables, $body);
+    }
+
+    public function createRemoteLink($issue, $options = array())
+    {
+        $path = '{+base_path}/issue/{+issue}/remotelink';
+        $variables = array(
+            'issue' => $issue,
+        );
+        $body = json_encode($options);
+        return $this->sendPost($path, $variables, $body);
+    }
 
 }
--- src/Jira/Common/JiraClient.php
+++ src/Jira/Common/JiraClient.php
@@ -6,5 +6,59 @@ use Guzzle\Service\Client;
 
 class JiraClient extends Client
 {
+    /**
+     * Helper method to send a GET request and return parsed JSON.
+     *
+     * @param string $path
+     * @param array $variables
+     *   Variables used to expand the URI expressions.
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @see http://docs.guzzlephp.org/en/latest/http-client/uri-templates.html
+     */
+    public function sendGet($path, $variables = array())
+    {
+        return $this->get(array($path, $variables))->send()->json();
+    }
 
+    /**
+     * Helper method to send a PUT request and return parsed JSON.
+     *
+     * @param string $path
+     * @param array $variables
+     *   Variables used to expand the URI expressions.
+     * @param string $body
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @see http://docs.guzzlephp.org/en/latest/http-client/uri-templates.html
+     */
+    public function sendPut($path, $variables = array(), $body)
+    {
+        return $this->put(array($path, $variables), null, $body)->send()->json();
+    }
+
+    /**
+     * Helper method to send a POST request and return parsed JSON.
+     *
+     * @param string $path
+     * @param array $variables
+     *   Variables used to expand the URI expressions.
+     * @param string $body
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @see http://docs.guzzlephp.org/en/latest/http-client/uri-templates.html
+     */
+    public function sendPost($path, $variables = array(), $body)
+    {
+        return $this->post(array($path, $variables), null, $body)->send()->json();
+    }
 }
