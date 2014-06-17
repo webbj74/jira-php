18ba3e52bbe9378f5bbed63d15f60f30a99b3cc6
Jonathan Webb <webbj2012@jwwebb.info>
2014-06-17 14:11:14 -0400

    Improved error handling for BadResponseException

--- src/Jira/Common/JiraClient.php
+++ src/Jira/Common/JiraClient.php
@@ -2,10 +2,46 @@
 
 namespace Jira\Common;
 
+use Guzzle\Http\Exception\BadResponseException;
 use Guzzle\Service\Client;
 
 class JiraClient extends Client
 {
+    /**
+     * Helper method to include HTTP response body in exception message.
+     */
+    protected function addBodyToBadResponseException(BadResponseException $badResponseException)
+    {
+        $request = $badResponseException->getRequest();
+        $response = $badResponseException->getResponse();
+
+        $jsonOptions = defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0;
+
+        // For POST requests include the body
+        $requestBody = '';
+        if ($request->getMethod() == 'POST') {
+            $requestBody = json_encode(json_decode($request->getBody(true)), $jsonOptions);
+        }
+
+        // Create an error message with body data included
+        $message = implode(
+            PHP_EOL,
+            array (
+                $badResponseException->getMessage(),
+                '[response body] ' . json_encode(json_decode($response->getBody(true)), $jsonOptions),
+                '[request body] ' . $requestBody,
+            )
+        );
+
+        // Rethrow the exception
+        $exceptionClass = get_class($badResponseException);
+        $badResponseExceptionWithBody = new $exceptionClass($message);
+        $badResponseExceptionWithBody->setResponse($response);
+        $badResponseExceptionWithBody->setRequest($request);
+
+        return $badResponseExceptionWithBody;
+    }
+
     /**
      * Helper method to send a GET request and return parsed JSON.
      *
@@ -21,7 +57,14 @@ class JiraClient extends Client
      */
     public function sendGet($path, $variables = array())
     {
-        return $this->get(array($path, $variables))->send()->json();
+        $response = null;
+        try {
+            $response = $this->get(array($path, $variables))->send();
+        } catch (BadResponseException $exception) {
+            throw $this->addBodyToBadResponseException($exception);
+        }
+
+        return $response->json();
     }
 
     /**
@@ -40,7 +83,14 @@ class JiraClient extends Client
      */
     public function sendPut($path, $variables = array(), $body)
     {
-        return $this->put(array($path, $variables), null, $body)->send()->json();
+        $response = null;
+        try {
+            $response = $this->put(array($path, $variables), null, $body)->send();
+        } catch (BadResponseException $exception) {
+            throw $this->addBodyToBadResponseException($exception);
+        }
+
+        return $response->json();
     }
 
     /**
@@ -59,6 +109,13 @@ class JiraClient extends Client
      */
     public function sendPost($path, $variables = array(), $body)
     {
-        return $this->post(array($path, $variables), null, $body)->send()->json();
+        $response = null;
+        try {
+            $response = $this->post(array($path, $variables), null, $body)->send();
+        } catch (BadResponseException $exception) {
+            throw $this->addBodyToBadResponseException($exception);
+        }
+
+        return $response->json();
     }
 }
